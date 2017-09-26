aa98a1a91c4bd2b8975598b40165ae8f0552c279
Dmitri Lakachauskis <lakiboy83@gmail.com>
2017-09-26 11:36:44 +0300

    Basic features.

--- bot.js
+++ bot.js
@@ -1,21 +1,36 @@
 const Telegraf = require('telegraf')
-const { memorySession } = require('telegraf')
+const axios = require('axios')
 
+const CURRENCIES = ['BTC', 'ETH', 'LTC']
+const FOLLOW_INTERVAL = 15 * 60 * 1000 // 15 minutes
+
+const http = axios.create({
+    baseURL: 'https://api.coinbase.com/v2',
+    timeout: 3000,
+    headers: { 'CB-VERSION': '2017-09-05' }
+})
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.use(memorySession())
+const fetchAnReply = async (currency, reply) => {
+    const { data: { data }} = await http.get(`/prices/${currency}-EUR/spot`)
 
-bot.use((ctx, next) => {
-    const start = new Date()
+    return reply(`*${data.base}*: ${data.amount} ${data.currency}`)
+}
 
-    return next().then(() => console.log('response time %sms', new Date() - start))
+bot.command('follow', ({ message: { text }, replyWithMarkdown }) => {
+    const currency = text.substr(7).trim()
+
+    if (!CURRENCIES.includes(currency)) {
+        return replyWithMarkdown(`Please type _/follow_ <${CURRENCIES.join(' | ')}>`)
+    }
+
+    setInterval(() => fetchAnReply(currency, replyWithMarkdown), FOLLOW_INTERVAL)
+
+    return replyWithMarkdown(`Following *${currency}*`)
 })
 
-bot.hears('Hey', ({ session, replyWithMarkdown }) => {
-    session.heyCounter = session.heyCounter || 0
-    session.heyCounter++
-
-    return replyWithMarkdown(`_Hey counter:_ ${session.heyCounter}`)
-})
+bot.hears('BTC', ({ replyWithMarkdown }) => fetchAnReply('BTC', replyWithMarkdown))
+bot.hears('ETH', ({ replyWithMarkdown }) => fetchAnReply('ETH', replyWithMarkdown))
+bot.hears('LTC', ({ replyWithMarkdown }) => fetchAnReply('LTC', replyWithMarkdown))
 
 bot.startPolling()
